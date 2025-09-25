308aafab7a7880ff77497b50498ce398d6784a3e
MartinCesano <cesano305@gmail.com>
2025-09-25 06:59:37 +0000

    Variables de entorno hardcodeadas para local

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,2 +1,4 @@
-export const BACKEND_URL = 'https://calculadora-imc-backend-hzhme0ghaxcfbrd0.brazilsouth-01.azurewebsites.net';
-export const AUTH_API_URL = `https://user-auth-g9d4a5emfjgkg4ak.brazilsouth-01.azurewebsites.net`;
+// export const BACKEND_URL = 'https://calculadora-imc-backend-hzhme0ghaxcfbrd0.brazilsouth-01.azurewebsites.net';
+// export const AUTH_API_URL = `https://user-auth-g9d4a5emfjgkg4ak.brazilsouth-01.azurewebsites.net`;
+export const BACKEND_URL = 'http://localhost:3000';
+export const AUTH_API_URL = `http://localhost:3001`; // Cambiado para desarrollo local
